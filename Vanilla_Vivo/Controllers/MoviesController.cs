4e1fa111d7df0bfab108922e4898730a5a3bebca
ashiq97 <shishir4097@gmail.com>
2018-07-12 18:15:22 +0600

    added movie model

---
+++ Vanilla_Vivo/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vanilla_Vivo.Models;
+
+namespace Vanilla_Vivo.Controllers
+{
+	public class MoviesController : Controller
+	{
+		//
+		// GET: /Movies/
+		public ActionResult Random()
+		{
+            var movie = new Movie()
+            {
+                Name = "Sherk!"
+            };
+			return View(movie);
+		}
+	}
+}
---
+++ Vanilla_Vivo/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vanilla_Vivo.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
