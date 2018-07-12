2c343a3b3b942027143442f4c1b28ae663ab427a
ashiq97 <shishir4097@gmail.com>
2018-07-12 21:34:52 +0600

    section 2, content without exercise

--- Vanilla_Vivo/Controllers/MoviesController.cs
+++ Vanilla_Vivo/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vanilla_Vivo.Models;
+using Vanilla_Vivo.ViewModels;
 
 namespace Vanilla_Vivo.Controllers
 {
@@ -11,13 +12,67 @@ namespace Vanilla_Vivo.Controllers
 	{
 		//
 		// GET: /Movies/
-		public ActionResult Random()
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() 
+            {
+                Name = "Sherk!"
+            };
+
+            var customers = new List<Customer>
+            {
+               new Customer{Name = "Customer 1"},
+               new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            //ViewBag.movieName = movie;
+            return View(viewModel);
+        }
+
+
+
+		/*public ActionResult Random()
 		{
             var movie = new Movie()
             {
                 Name = "Sherk!"
             };
-			return View(movie);
-		}
+            //return View(movie);
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+
+            return RedirectToAction("Index", "Home", new {page = 1,sortBy = "name"});
+        }
+        */
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id = " + id);
+        //}
+
+        //public ActionResult Index(int? pageIndex,string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "name";
+
+        //    return Content(String.Format("PageIndex = {0} & SortBy = {1}",pageIndex,sortBy));
+        //}
+
+        /*useing attribute route
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+
+            return Content(year + "/" + month);
+        }
+        */
 	}
 }
---
+++ Vanilla_Vivo/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vanilla_Vivo.Models;
+
+namespace Vanilla_Vivo.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
